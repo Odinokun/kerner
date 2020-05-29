af6ebc7eab493fa353eedecc3820670d04bb815a
odinokun <odinokun@gmail.com>
2020-05-29 21:49:23 +0300

    did desktop basket

--- src/js/modules/slick.js
+++ src/js/modules/slick.js
@@ -129,6 +129,33 @@ module.exports = function() {
       }
     ]
   });
+
+
+  $('#card-body-slider-lg__list').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    adaptiveHeight: true,
+    asNavFor: '#card-body-slider-sm__list'
+  });
+  $('#card-body-slider-sm__list').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    vertical: true,
+    verticalSwiping: true,
+    asNavFor: '#card-body-slider-lg__list',
+    prevArrow: "<div class='prev'><svg width=\"7\" height=\"12\" viewBox=\"0 0 7 12\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M6 1L2 6L6 11\" stroke=\"white\" stroke-width=\"2\"/></svg></div>",
+    nextArrow: "<div class='next'><svg width=\"7\" height=\"12\" viewBox=\"0 0 7 12\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M1 11L5 6L1 1\" stroke=\"white\" stroke-width=\"2\"/></svg></div>",
+    dots: true,
+    infinite: false,
+    centerMode: true,
+    focusOnSelect: true
+  });
+
+
+
+
   // end Slick slider
 
 };
